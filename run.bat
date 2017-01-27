34d14a982fac0a7d28fe388983e794b8ee1ee956
2229695w <2229695w@student.gla.ac.uk>
2017-01-27 16:36:20 +0000

    Ch7 done

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-python E:\workspace\WAD2\tango_with_django_project\manage.py runserver 127.0.0.1:8000
+python .\manage.py runserver 127.0.0.1:8000
+cmd /k
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -40,9 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
-    'templates',
-    'static',
-    'media',
 ]
 
 MIDDLEWARE = [
